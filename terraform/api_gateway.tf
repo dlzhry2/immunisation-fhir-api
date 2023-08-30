18c23652a9e308fe08ff4aaf5e67cb745bc91876
almas-liaqat <Almas.liaqat@accentre.com>
2023-08-30 14:57:59 +0100

    removed api gateway module + added s3 bucket

--- terraform/api_gateway.tf
+++
@@ -1,9 +0,0 @@
-module "api_gateway" {
-  source = "./api_gateway"
-
-  prefix          = local.prefix
-  short_prefix    = local.short_prefix
-  zone_id         = aws_route53_zone.project_zone.zone_id
-  api_domain_name = local.service_domain_name
-  environment     = local.environment
-}
