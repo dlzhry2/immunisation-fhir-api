6d8778b184d8aa74165593861ba1db25d39f959d
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-09-05 14:43:45 +0100

    AMB-1688: Fixed destination bucket name

--- terraform/batch-processing/src/batch_processing.py
+++ terraform/batch-processing/src/batch_processing.py
@@ -6,23 +6,22 @@ from boto3 import resource
 
 
 def lambda_handler(_event, _context):
-    s3_resource = resource('s3')
-    output_bucket = s3_resource.Bucket("destination")
+    source_bucket_name = _event.get("Records")[0].get("s3").get("bucket").get("name")
+    dest_bucket_name = source_bucket_name.replace("source", "destination")
+    output_bucket = resource('s3').Bucket(dest_bucket_name)
 
     # Create a temporary dir to write our output file to, this avoids polluting /tmp with test files.
     # Using the context manager here ensures the temp dir is cleaned up when the with statement is closed.
     with tempfile.TemporaryDirectory() as tmpdir:
         os.chdir(tmpdir)
 
-        # Write some placeholder data to a file in the bucket,
+        # Write some placeholder bytestring data to a file in the bucket,
         # so we can test that the lambda writes to the correct output bucket.
         filename = f"output_report_{time()}.txt"
-        data = 'Test file to see if the lambda writes to the correct s3 bucket. '
-        'If our AWS bill skyrockets, this file has been written to the wrong bucket!'
+        data = (b'Test file to see if the lambda writes to the correct s3 bucket. '
+                b'If our AWS bill skyrockets, this file has been written to the wrong bucket!')
 
-        # Upload the output file to our destination S3 bucket
-        file_obj = BytesIO(data)
-        output_bucket.upload_fileobj(file_obj, output_bucket, filename)
+        output_bucket.put_object(Body=data, Key=filename)
 
     return {
         'statusCode': 200
