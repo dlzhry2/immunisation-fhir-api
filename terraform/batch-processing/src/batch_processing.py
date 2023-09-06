fd505ff896a84a28bb0822e8092f4d6bcd24abdd
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2023-09-06 13:57:04 +0100

    AMB-1688: Removed redundant tempfile context manager

--- terraform/batch-processing/src/batch_processing.py
+++ terraform/batch-processing/src/batch_processing.py
@@ -9,18 +9,13 @@ def lambda_handler(_event, _context):
     dest_bucket_name = source_bucket_name.replace("source", "destination")
     output_bucket = resource('s3').Bucket(dest_bucket_name)
 
-    # Create a temporary dir to write our output file to, this avoids polluting /tmp with test files.
-    # Using the context manager here ensures the temp dir is cleaned up when the with statement is closed.
-    with tempfile.TemporaryDirectory() as tmpdir:
-        os.chdir(tmpdir)
+    # Write some placeholder bytestring data to a file in the bucket,
+    # so we can test that the lambda writes to the correct output bucket.
+    filename = f"output_report_{time()}.txt"
+    data = (b'Test file to see if the lambda writes to the correct s3 bucket. '
+            b'If our AWS bill skyrockets, this file has been written to the wrong bucket!')
 
-        # Write some placeholder bytestring data to a file in the bucket,
-        # so we can test that the lambda writes to the correct output bucket.
-        filename = f"output_report_{time()}.txt"
-        data = (b'Test file to see if the lambda writes to the correct s3 bucket. '
-                b'If our AWS bill skyrockets, this file has been written to the wrong bucket!')
-
-        output_bucket.put_object(Body=data, Key=filename)
+    output_bucket.put_object(Body=data, Key=filename)
 
     return {
         'statusCode': 200
