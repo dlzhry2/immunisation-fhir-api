6c0c69dfe06dd2a8e198a2c56630c5b6dc645859
artronics <jalalhosseiny@gmail.com>
2023-09-12 20:34:25 +0100

    AMB-1697 happy-path test will cover auth methods. Remove specific auth tests.

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -3,64 +3,67 @@ See
 https://github.com/NHSDigital/pytest-nhsd-apim/blob/main/tests/test_examples.py
 for more ideas on how to test the authorization of your API.
 """
-import json
 import random
 import time
+
 import pytest
 import requests
 from assertpy import assert_that
 
 
-@pytest.mark.debug
-@pytest.mark.smoketest
-@pytest.mark.nhsd_apim_authorization(
-        {
-            "access": "healthcare_worker",
-            "level": "aal3",
-            "login_form": {"username": "656005750104"},
-        }
-    )
-def test_lambda_crud(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    """
-    Test for the POST,GET and Delete for Lambda endpoints.
-    """
+def lambda_crud(url, headers):
     timestamp = int(time.time())
     random.seed(timestamp)
-    id = str(random.randint(0, 1000))
-    expected_post_status_code = 201
-    expected_delete_status_code = 200
-    expected_get_status_code = 200
-    headers = {
-                "Content-Type": "application/json"
-    }
-    request_payload = {
-                      "id": id,
-                      "message": "Hello World"}
-    json_payload = json.dumps(request_payload)
+    event_id = str(random.randint(0, 1000))
 
-    headers.update(nhsd_apim_auth_headers)
+    request_payload = {
+        "id": event_id,
+        "message": "Hello World"}
     post_response = requests.post(
-            url=f"{nhsd_apim_proxy_url}/event",
-            headers=headers,
-            data=json_payload
-        )
+        url=f"{url}/event",
+        headers=headers,
+        json=request_payload,
+    )
+    assert_that(201).is_equal_to(post_response.status_code)
 
     get_response = requests.get(
-            url=f"{nhsd_apim_proxy_url}/event",
-            headers=headers,
-            params={
-                "id": id
-            }
-        )
+        url=f"{url}/event",
+        headers=headers,
+        params={
+            "id": event_id
+        }
+    )
+    assert_that(200).is_equal_to(get_response.status_code)
 
     delete_response = requests.delete(
-            url=f"{nhsd_apim_proxy_url}/event",
-            headers=headers,
-            params={
-                "id": id
-            }
-        )
+        url=f"{url}/event",
+        headers=headers,
+        params={
+            "id": event_id
+        }
+    )
+    assert_that(200).is_equal_to(delete_response.status_code)
 
-    assert_that(expected_post_status_code).is_equal_to(post_response.status_code)
-    assert_that(expected_delete_status_code).is_equal_to(delete_response.status_code)
-    assert_that(expected_get_status_code).is_equal_to(get_response.status_code)
+
+@pytest.mark.smoketest
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "healthcare_worker",
+        "level": "aal3",
+        "login_form": {"username": "656005750104"},
+    }
+)
+def test_lambda_crud_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    """
+    Test for the POST,GET and Delete for Lambda endpoints. Using user-restricted auth
+    """
+    lambda_crud(nhsd_apim_proxy_url, nhsd_apim_auth_headers)
+
+
+@pytest.mark.smoketest
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
+def test_lambda_crud_app_restricted(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    """
+    Test for the POST,GET and Delete for Lambda endpoints. Using app-restricted
+    """
+    lambda_crud(nhsd_apim_proxy_url, nhsd_apim_auth_headers)
