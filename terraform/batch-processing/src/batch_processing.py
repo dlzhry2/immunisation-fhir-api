0e4e537f38cc10dd17bf8a3c8ea2891a15205b60
ewan-IW <ewan.childs@infinityworks.com>
2023-09-12 17:06:16 +0100

    Reads from S3 bucket and puts to api-gateway with response codes

--- terraform/batch-processing/src/batch_processing.py
+++ terraform/batch-processing/src/batch_processing.py
@@ -1,20 +1,55 @@
 from time import time
 from boto3 import resource
+import requests
+import json
 
 
 def lambda_handler(_event, _context):
     source_bucket_name = _event.get("Records")[0].get("s3").get("bucket").get("name")
     dest_bucket_name = source_bucket_name.replace("source", "destination")
     output_bucket = resource('s3').Bucket(dest_bucket_name)
+    api_gateway_url = "https://hphlz3bvpb.execute-api.eu-west-2.amazonaws.com/alli5"
+    
+    #getting JSON object
+    s3_record = _event["Records"][0]["s3"]
+    object_key = s3_record["object"]["key"]
+    
+    #download JSON object from S3
+    s3_object_url = f"s3://{source_bucket_name}/{object_key}"
+    response = requests.get(s3_object_url)
+    
+    #send JSON object to API-gateway if succesfully downloaded
+    if response.status_code == 200:
+        json_data = json.loads(response.text)
+        api_response = requests.post(api_gateway_url, json=json_data)
+        filename = f"output_report_{time()}.txt"
+        output_bucket.put_object(Body=json_data, Key=filename)
+        
+        if api_response.status_code == 200:
+            
+            return {
+                'statusCode': 200,
+                'body': 'Successfully sent JSON data to the API Gateway.'
+            }
+        else:
+            return {
+                'statusCode': api_response.status_code,
+                'body': 'Error sending JSON data to the API Gateway.'
+            }
+    else:
+        return {
+            'statusCode': response.status_code,
+            'body': 'Error fetching JSON object from S3.'
+        }
 
     # Write some placeholder bytestring data to a file in the bucket,
     # so we can test that the lambda writes to the correct output bucket.
-    filename = f"output_report_{time()}.txt"
-    data = (b'Test file to see if the lambda writes to the correct s3 bucket. '
-            b'If our AWS bill skyrockets, this file has been written to the wrong bucket!')
+    # filename = f"output_report_{time()}.txt"
+    # data = (b'Test file to see if the lambda writes to the correct s3 bucket. '
+    #         b'If our AWS bill skyrockets, this file has been written to the wrong bucket!')
 
-    output_bucket.put_object(Body=data, Key=filename)
+    # output_bucket.put_object(Body=data, Key=filename)
 
-    return {
-        'statusCode': 200
-    }
+    # return {
+    #     'statusCode': 200
+    # }
