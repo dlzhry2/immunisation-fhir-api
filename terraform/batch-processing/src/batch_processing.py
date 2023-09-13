5514896d79b9991f1fedd28f5af60fab99d89290
ewan-IW <ewan.childs@infinityworks.com>
2023-09-13 11:00:54 +0100

    Fixing more linting errors and passing body parameter to payload output

--- terraform/batch-processing/src/batch_processing.py
+++ terraform/batch-processing/src/batch_processing.py
@@ -11,15 +11,15 @@ def lambda_handler(_event, _context):
     output_bucket = resource('s3').Bucket(dest_bucket_name)
     api_gateway_url = "https://hphlz3bvpb.execute-api.eu-west-2.amazonaws.com/alli5"
 
-#getting JSON object
+# getting JSON object
     s3_record = _event["Records"][0]["s3"]
     object_key = s3_record["object"]["key"]
 
-#download JSON object from S3
+# download JSON object from S3
     s3_object_url = f"s3://{source_bucket_name}/{object_key}"
     response = requests.get(s3_object_url)
 
-#send JSON object to API-gateway if succesfully downloaded
+# send JSON object to API-gateway if succesfully downloaded
     if response.status_code == 200:
         json_data = json.loads(response.text)
         filename = f"output_report_{time()}.txt"
@@ -27,23 +27,25 @@ def lambda_handler(_event, _context):
 # Send data to the API Gateway
         api_response = requests.post(api_gateway_url, json=payload_json)
 
-#Construct JSON object to be sent to logged to output bucket
+# Construct JSON object to be sent to logged to output bucket
+        request_id = str(uuid.uuid4())
+        
         payload = {
             "timestamp": f"output_report_{time()}.txt",
             "level": json_data,
             "request": {
-                "x-request-id": str(uuid.uuid4()),
-                "x-correlation-id": ""
+                "x-request-id": request_id,
+                "x-correlation-id": request_id
             }
         }
 
-#Convert payload to JSON string
+# Convert payload to JSON string
         payload_json = json.dumps(payload)
 
-#Send payload as JSON string to output bucket
-        output_bucket.put_object(Body=json_data, Key=filename)
+# Send payload as JSON string to output bucket
+        output_bucket.put_object(Body=payload_json, Key=filename)
 
-#return status codes depending on api_response
+# return status codes depending on api_response
         if api_response.status_code == 200:
             return {
                 'statusCode': 200,
@@ -59,7 +61,7 @@ def lambda_handler(_event, _context):
             'statusCode': response.status_code,
             'body': 'Error fetching JSON object from S3.'
         }
-
+        
 # Write some placeholder bytestring data to a file in the bucket,
 # so we can test that the lambda writes to the correct output bucket.
 # filename = f"output_report_{time()}.txt"
