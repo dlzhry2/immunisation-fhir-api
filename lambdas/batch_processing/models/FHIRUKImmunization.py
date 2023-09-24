c09b6494fd4fb1698d662ca74e60988b598b9b7b
almas-liaqat <Almas.liaqat@accentre.com>
2023-09-24 02:01:12 +0100

    fixed lint

--- lambdas/batch_processing/models/FHIRUKImmunization.py
+++ lambdas/batch_processing/models/FHIRUKImmunization.py
@@ -6,45 +6,44 @@ from fhir.resources.practitioner import Practitioner as FHIRPractitioner
 import datetime
 import json
 
-class UKFHIRImmunization():
+
+class UKFHIRImmunization:
     def __init__(self, *args, **kwargs):
 
-        self.immunization:FHIRImmunization = None
-        self.patient:FHIRPatient = None
-        self.recorded:datetime.date = None     
-        self.reportOrigin:CodeableConcept= None
-        self.reasonCode:CodeableConcept= []
-        self.manufacturer:FHIROrganization= None
-        self.actor:FHIRPractitioner =None
+        self.immunization: FHIRImmunization = None
+        self.patient: FHIRPatient = None
+        self.recorded: datetime.date = None
+        self.reportOrigin: CodeableConcept = None
+        self.reasonCode: CodeableConcept = []
+        self.manufacturer: FHIROrganization = None
+        self.actor: FHIRPractitioner = None
 
     def get_immunization(self):
-        return self.immunization.json()    
-        
+        return self.immunization.json()
+
     def get_patient(self):
         return self.patient.json()
-    
+
     def get_manufacturer(self):
         return self.manufacturer.json()
 
     def get_actor(self):
-        return {"actor": json.loads(self.actor.json())}  #Matching representation to that of Expected JSON
-        
+        return {
+            "actor": json.loads(self.actor.json())
+        }  # Matching representation to that of Expected JSON
+
     def get_report_origin(self):
         return self.reportOrigin.json()
-        
+
     def get_reason_code(self):
         if not self.reasonCode:
             return []  # Return an empty list if self.reasonCode is empty
-        
+
         # Serialize each CodeableConcept object in the list to JSON
-        reason_code_json_list = [json.loads(reason_code.json()) for reason_code in self.reasonCode]
+        reason_code_json_list = [
+            json.loads(reason_code.json()) for reason_code in self.reasonCode
+        ]
         return json.dumps(reason_code_json_list)
-    
+
     def get_recorded(self):
         return self.recorded.isoformat() if self.recorded else None
-        
-
-   
-
-        
-         
