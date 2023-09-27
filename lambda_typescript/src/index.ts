8215f49b9e95978f96adf8a2afebfd3533656080
ewan-IW <ewan.childs@infinityworks.com>
2023-09-27 11:53:23 +0100

    AMB-1708 Adding status endpoint to back end lambda

--- lambda_typescript/src/index.ts
+++ lambda_typescript/src/index.ts
@@ -1,10 +1,21 @@
 import { DynamoDB } from 'aws-sdk';
 
+
 const dynamoDB = new DynamoDB.DocumentClient();
 
 export const handler = async (event: any): Promise<any> => {
   const operation = event.httpMethod;
   const dynamoDBTableName = process.env.DYNAMODB_TABLE_NAME || 'DefaultTableName';
+  if (event.path === '/_status' && operation === 'GET') {
+    const statusResponse = {
+      status: 'healthy',
+      message: 'Backend is up and running.',
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(statusResponse),
+    };
+  }
   
   if (operation === 'POST' && event.path === '/event') {
     // Handle the POST request to create a new item
@@ -12,7 +23,7 @@ export const handler = async (event: any): Promise<any> => {
       TableName: dynamoDBTableName,
       Item: JSON.parse(event.body),
     };
-
+    
     try {
       await dynamoDB.put(params).promise();
       return {
@@ -34,7 +45,6 @@ export const handler = async (event: any): Promise<any> => {
         id: event.queryStringParameters.id,
       },
     };
-
     try {
       const data = await dynamoDB.get(params).promise();
       if (!data.Item) {
@@ -63,7 +73,6 @@ export const handler = async (event: any): Promise<any> => {
         id: event.queryStringParameters.id,
       },
     };
-
     try {
         const data = await dynamoDB.get(params).promise();
         if (!data.Item) {
@@ -87,7 +96,6 @@ export const handler = async (event: any): Promise<any> => {
       };
     }
   }
-
   return {
     statusCode: 400,
     body: JSON.stringify('Invalid operation.'),
