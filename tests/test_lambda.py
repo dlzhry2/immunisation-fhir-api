2bde6aacdcc1cdc0f777f256ac1dc5df60d2edcb
ewan-IW <ewan.childs@infinityworks.com>
2023-09-27 13:13:18 +0100

    Fixing linting errors

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -9,19 +9,19 @@ def lambda_crud(url, headers):
     timestamp = int(time.time())
     random.seed(timestamp)
     event_id = str(random.randint(0, 1000))
-    
+
     request_payload = {
         "id": event_id,
         "message": "Hello World"
     }
-    
+
     post_response = requests.post(
         url=f"{url}/event",
         headers=headers,
         json=request_payload,
     )
     assert_that(201).is_equal_to(post_response.status_code)
-    
+
     get_response = requests.get(
         url=f"{url}/event",
         headers=headers,
@@ -30,7 +30,7 @@ def lambda_crud(url, headers):
         }
     )
     assert_that(200).is_equal_to(get_response.status_code)
-    
+
     delete_response = requests.delete(
         url=f"{url}/event",
         headers=headers,
@@ -39,8 +39,8 @@ def lambda_crud(url, headers):
         }
     )
     assert_that(200).is_equal_to(delete_response.status_code)
-    
-    
+
+
 @pytest.mark.smoketest
 @pytest.mark.nhsd_apim_authorization(
     {
@@ -54,12 +54,12 @@ def test_lambda_crud_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     Test for the POST,GET and Delete for Lambda endpoints. Using user-restricted auth
     """
     lambda_crud(nhsd_apim_proxy_url, nhsd_apim_auth_headers)
-    
-    
+
+
 @pytest.mark.smoketest
 @pytest.mark.nhsd_apim_authorization(
     {
-        "access": "application", 
+        "access": "application",
         "level": "level3"
      })
 def test_lambda_crud_app_restricted(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
