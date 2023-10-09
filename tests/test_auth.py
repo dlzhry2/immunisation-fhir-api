35b59888e46c57cd51eb472ff46482b70a65ba47
ewan-IW <ewan.childs@infinityworks.com>
2023-10-09 23:49:11 +0100

    linting

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -52,7 +52,7 @@ def test_invalid_access_token():
 def test_invalid_endpoint_returns_404(nhsd_apim_auth_headers):
     expected_status_code = 404
     expected_body = load_example("OperationOutcome/404-not_found.json")
-     
+
     response = requests.get(url=f"{config.BASE_URL}/{config.BASE_PATH}/invalid_url", headers=nhsd_apim_auth_headers)
 
     assert response.status_code == expected_status_code
