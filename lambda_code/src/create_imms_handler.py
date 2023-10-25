af36324ffdf8a94739e3180dbb6fd8d499ce80f7
nathanwall <me+github@nathanwall.co.uk>
2023-10-25 10:45:04 +0100

    tidy up

--- lambda_code/src/create_imms_handler.py
+++ lambda_code/src/create_imms_handler.py
@@ -1,13 +1,9 @@
-import os
-
-import boto3
 import json
 from dynamodb import EventTable
 
 
 def create_imms_handler(event, context):
     print(json.dumps(event))
-
     event_body = json.loads(event["body"])
     print(event_body)
     dynamo_service = EventTable()
