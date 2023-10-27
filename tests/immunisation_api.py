31c2856c81324546ef91c1858e1ada5f4f1e766a
artronics <jalalhosseiny@gmail.com>
2023-10-27 11:41:17 +0100

    AMB-1741 move imms api in one place

--- tests/immunisation_api.py
+++
@@ -1,9 +0,0 @@
-class ImmunisationApi:
-
-    def __init__(self, url, token):
-        self.url = url
-        self.token = token
-
-    def get_event_by_id(self, event_id):
-        # Make your request to our api here and return the response
-        return "response"
--- tests/test_immunisation_api.py
+++ tests/test_immunisation_api.py
@@ -1,6 +1,15 @@
 import pytest
 
-from immunisation_api import ImmunisationApi
+
+class ImmunisationApi:
+
+    def __init__(self, url, token):
+        self.url = url
+        self.token = token
+
+    def get_event_by_id(self, event_id):
+        # Make your request to our api here and return the response
+        return "response"
 
 
 # TODO: send a GET /event/{id} request
