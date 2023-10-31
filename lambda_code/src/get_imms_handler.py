cee36a2ec05ac884b8c4eed15a7338de5f247b5d
ewan-IW <ewan.childs@infinityworks.com>
2023-10-31 15:24:40 +0000

    missing file from last commit

--- lambda_code/src/get_imms_handler.py
+++ lambda_code/src/get_imms_handler.py
@@ -6,15 +6,15 @@ import re
 def get_imms_handler(event, context):
     dynamo_service = EventTable()
     return get_imms(event, dynamo_service)
-    
+
 # create function which recieves event and instance of dynamodb
 def get_imms(event, dynamo_service):
     event_id = event["pathParameters"]["id"]
-    
+
     def is_valid_id(event_id):
         pattern = r'^[A-Za-z0-9\-.]{1,64}$'
         return re.match(pattern, event_id) is not None
-    
+
     if not is_valid_id(event_id) or not event_id:
         return {
         "statusCode": 400,
@@ -46,7 +46,7 @@ def get_imms(event, dynamo_service):
             ]
         })
     }
-    
+
     message = dynamo_service.get_event_by_id(event_id)
     if message is None:
         return {
@@ -79,9 +79,8 @@ def get_imms(event, dynamo_service):
             ]
         })
     }
-    
+
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
-    
