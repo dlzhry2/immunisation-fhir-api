bf132f2fdf10181a3e710f3d4b6a783af22b5d97
ewan-IW <ewan.childs@infinityworks.com>
2023-11-26 21:39:46 +0000

    Changing requirements.txt and terrafomr makefile

--- lambda_code/src/create_imms_handler.py
+++ lambda_code/src/create_imms_handler.py
@@ -1,14 +1,14 @@
 import json
 from dynamodb import EventTable
-from get_secret import SecretsManagerSecret
-import boto3
+from pds import PdsService
 
 
 def create_imms_handler(event, context):
     event_body = event["body"]
-    secrets_manager_client = boto3.client('secretsmanager')
-    secret_service = SecretsManagerSecret(secrets_manager_client)
-    secret = secret_service.get_value()
+    nhs_number = event_body["nhs_number"]
+    pds_service = PdsService()
+    patient_response = pds_service.get_patient_details(nhs_number)
+    print(patient_response, "<<<<<<<<<<<, PATIENT_RESPONSE")
     print(event_body, "<<<<<<<<<<<< EVENT_BODY")
     dynamo_service = EventTable()
     message = dynamo_service.put_event(event_body)
