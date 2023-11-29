a18bdc24f91a8da9bb3649cac73f497d112c754c
ewan-IW <ewan.childs@infinityworks.com>
2023-11-29 14:04:40 +0000

    Comitting files to compare with main

--- lambda_code/entrypoint.sh
+++ lambda_code/entrypoint.sh
@@ -11,6 +11,14 @@ python -m unittest
 cd /lambda_package || exit 2
 echo "Installing main dependencies..."
 pip install -q -r requirements.txt --target .
+echo "Building cryptography layer"
+pip install \
+--platform manylinux2014_x86_64 \
+--target=. \
+--implementation cp \
+--python-version 3.10 \
+--only-binary=:all: --upgrade \
+'cryptography==36.0.1'
 echo "Building lambda package"
 zip -qq -r /build/lambda_package.zip .
 echo "Done!"
--- lambda_code/src/create_imms_handler.py
+++ lambda_code/src/create_imms_handler.py
@@ -1,22 +1,31 @@
-import json
-from dynamodb import EventTable
+# import json
+# from dynamodb import EventTable
 from pds import PdsService
+import platform
 
 
 def create_imms_handler(event, context):
-    event_body = event["body"]
-    nhs_number = event_body["nhs_number"]
-    pds_service = PdsService()
-    patient_response = pds_service.get_patient_details(nhs_number)
-    print(patient_response, "<<<<<<<<<<<, PATIENT_RESPONSE")
-    print(event_body, "<<<<<<<<<<<< EVENT_BODY")
-    event_body = json.loads(event["body"])
-    dynamo_service = EventTable()
-    message = dynamo_service.put_event(event_body)
-    response = {
-        "statusCode": 201,  # HTTP status code
-        "body": json.dumps({
-            "message": message  
-        })
+    architecture = platform.machine()
+    print(f"Lambda architecture: {architecture}")
+    return {
+        'statusCode': 200,
+        'body': f"Lambda architecture: {architecture}"
     }
-    return response
+    # event_body = event["body"]
+    # nhs_number = event_body["nhs_number"]
+    # pds_service = PdsService()
+    # patient_response = pds_service.get_patient_details(nhs_number)
+    # print(patient_response, "<<<<<<<<<<<, PATIENT_RESPONSE")
+    # print(event_body, "<<<<<<<<<<<< EVENT_BODY")
+    # conditional = None
+    
+    # if conditional:
+    #     dynamo_service = EventTable()
+    #     message = dynamo_service.put_event(event_body)
+    #     response = {
+    #         "statusCode": 201,  # HTTP status code
+    #         "body": json.dumps({
+    #             "message": message  
+    #         })
+    #     }
+    #     return response
