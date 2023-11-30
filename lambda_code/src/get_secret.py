e8345ba940114946c5f1bb6012da1769ba9810be
ewan-IW <ewan.childs@infinityworks.com>
2023-11-30 11:45:44 +0000

    Removing comments

--- lambda_code/src/get_secret.py
+++ lambda_code/src/get_secret.py
@@ -33,11 +33,3 @@ class SecretsManagerSecret:
             raise
         else:
             return response
-
-# Usage example for SecretsManagerCache. For future use (if needed)
-# class SecretsManagerCache:
-#     client = botocore.session.get_session().create_client('secretsmanager')
-#     cache_config = SecretCacheConfig()
-#     cache = SecretCache( config = cache_config, client = client)
-
-#     secret = cache.get_secret_string('imms/pds_private_key')
