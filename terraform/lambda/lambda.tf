f4dfae888866f29e4184c70ad3f396e5d1a24a21
Lee Richardson <1@example.com>
2023-12-08 16:19:53 +0000

    Use lambda role defined elsewhere

--- terraform/lambda/lambda.tf
+++ terraform/lambda/lambda.tf
@@ -24,6 +24,7 @@
 module "lambda_function_container_image" {
     source = "terraform-aws-modules/lambda/aws"
 
+    create_role = false
     lambda_role = aws_iam_role.lambda_role.arn
     function_name = "${var.short_prefix}_${var.function_name}"
     handler       = "${var.function_name}_handler.${var.function_name}_handler"
