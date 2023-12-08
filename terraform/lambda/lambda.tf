9972bfb41ca7936ca32678200103e60389f83fbc
Lee Richardson <1@example.com>
2023-12-08 15:17:09 +0000

    Attempt to fix lambda permissions

--- terraform/lambda/lambda.tf
+++ terraform/lambda/lambda.tf
@@ -24,18 +24,15 @@
 module "lambda_function_container_image" {
     source = "terraform-aws-modules/lambda/aws"
 
+    lambda_role = aws_iam_role.lambda_role.arn
     function_name = "${var.short_prefix}_${var.function_name}"
-    description   = "splab"
     handler       = "${var.function_name}_handler.${var.function_name}_handler"
 
     create_package = false
-
     image_uri    = var.image_uri
     package_type = "Image"
     architectures = ["x86_64"]
 
-    #docker_entrypoint = "echo "
     environment_variables = var.environments
-    #use_existing_cloudwatch_log_group = true
     image_config_command = ["${var.function_name}_handler.${var.function_name}_handler"]
 }
