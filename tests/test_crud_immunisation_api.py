80289c9e7696b487af23b3581b2c5716148aa28a
artronics <jalalhosseiny@gmail.com>
2024-01-26 16:47:06 +0000

    AMB-1742 add e2e test for inconsistent id and UPDATE after DELETE

--- tests/test_crud_immunisation_api.py
+++ tests/test_crud_immunisation_api.py
@@ -34,38 +34,6 @@ def create_a_deleted_imms_resource(imms_api: ImmunisationApi) -> dict:
         "login_form": {"username": "656005750104"},
     }
 )
-def test_update_immunization_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
-    token = nhsd_apim_auth_headers["Authorization"]
-    imms_api = ImmunisationApi(nhsd_apim_proxy_url, token)
-
-    imms = create_an_imms_obj()
-    result = imms_api.create_immunization(imms)
-    res_body = result.json()
-
-    assert result.status_code == 201
-
-    # UPDATE
-    imms_id = res_body['id']
-    new_imms = copy.deepcopy(imms)
-    new_imms["status"] = "not-done"
-    result = imms_api.update_immunization(imms_id, new_imms)
-    assert result.status_code == 200
-
-    # read back
-    result = imms_api.get_immunization_by_id(imms_id)
-    res_body = result.json()
-
-    assert result.status_code == 200
-    assert res_body["status"] == "not-done"
-
-
-@pytest.mark.nhsd_apim_authorization(
-    {
-        "access": "healthcare_worker",
-        "level": "aal3",
-        "login_form": {"username": "656005750104"},
-    }
-)
 def test_crud_immunization_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     token = nhsd_apim_auth_headers["Authorization"]
     imms_api = ImmunisationApi(nhsd_apim_proxy_url, token)
@@ -89,10 +57,13 @@ def test_crud_immunization_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers
 
     # UPDATE
     new_imms = copy.deepcopy(imms)
+    new_imms["id"] = imms_id
     new_imms["status"] = "not-done"
     result = imms_api.update_immunization(imms_id, new_imms)
+
     assert result.status_code == 200
 
+    # read it back
     result = imms_api.get_immunization_by_id(imms_id)
     res_body = result.json()
     assert res_body["status"] == "not-done"
@@ -200,18 +171,71 @@ def test_get_deleted_immunization(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
         "login_form": {"username": "656005750104"},
     }
 )
-@pytest.mark.debug
 def test_update_none_existing_record_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
     """update a record that doesn't exist should create a new record"""
     token = nhsd_apim_auth_headers["Authorization"]
     imms_api = ImmunisationApi(nhsd_apim_proxy_url, token)
 
     imms_id = str(uuid.uuid4())
-    # imms_id = "d5d9d3db-38d3-4765-a131-d372ca8812bf"
     imms = create_an_imms_obj(imms_id)
     result = imms_api.update_immunization(imms_id, imms)
-    # result = imms_api.create_immunization(imms)
-    print(result.text)
+    assert result.status_code == 201
+
+
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "healthcare_worker",
+        "level": "aal3",
+        "login_form": {"username": "656005750104"},
+    }
+)
+def test_update_inconsistent_id_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    """update should fail if id in the path doesn't match with the id in the message"""
+    token = nhsd_apim_auth_headers["Authorization"]
+    imms_api = ImmunisationApi(nhsd_apim_proxy_url, token)
+
+    msg_id = str(uuid.uuid4())
+    imms = create_an_imms_obj(msg_id)
+
+    path_id = str(uuid.uuid4())
+    result = imms_api.update_immunization(path_id, imms)
+    json = result.json()
+
+    assert result.status_code == 400
+    assert json["resourceType"] == "OperationOutcome"
+    assert path_id in json["issue"][0]["diagnostics"]
+
+
+@pytest.mark.nhsd_apim_authorization(
+    {
+        "access": "healthcare_worker",
+        "level": "aal3",
+        "login_form": {"username": "656005750104"},
+    }
+)
+@pytest.mark.debug
+def test_update_deleted_imms_nhs_login(nhsd_apim_proxy_url, nhsd_apim_auth_headers):
+    """updating deleted record will undo the delete"""
+    # This behaviour is consistent. Getting a deleted record will result in a 404. An update of a non-existent record
+    #  should result in creating a new record
+    token = nhsd_apim_auth_headers["Authorization"]
+    imms_api = ImmunisationApi(nhsd_apim_proxy_url, token)
+
+    imms = create_an_imms_obj()
+
+    # first create and then delete
+    result = imms_api.create_immunization(imms)
+    assert result.status_code == 201
+
+    imms_id = result.json()["id"]
+    result = imms_api.delete_immunization(imms_id)
+    assert result.status_code == 200
+
+    # When
+    imms["id"] = imms_id
+    result = imms_api.update_immunization(imms_id, imms)
+
+    # Then
     assert result.status_code == 201
 
 
