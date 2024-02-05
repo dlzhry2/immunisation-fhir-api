394d479a82a79ed9ee6d4b2d7dd1685f0bf02df8
AlexandraBenson <alexandra.benson3@nhs.net>
2024-02-05 17:44:19 +0000

    Fix tests by loading in floats as decimals

--- tests/example_loader.py
+++ tests/example_loader.py
@@ -1,5 +1,6 @@
 import json
 import os
+from decimal import Decimal
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -7,6 +8,6 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 def load_example(path: str):
     with open(f"{current_directory}/../specification/components/examples/{path}") as f:
         if path.endswith("json"):
-            return json.load(f)
+            return json.load(f, parse_float=Decimal)
         else:
             return f.read().strip()
