2eb3773d7d4edef3fa94b95f5dd513c099bddc21
artronics <jalalhosseiny@gmail.com>
2024-02-25 00:01:43 +0000

    AMB-1895 add authorization test failure test

--- e2e/test_authorization.py
+++ e2e/test_authorization.py
@@ -1,25 +1,22 @@
 import unittest
 import uuid
-from typing import List
+from typing import Set
 
 from lib.apigee import ApigeeService, ApigeeApp, ApigeeProduct
 from lib.authentication import AppRestrictedAuthentication
 from lib.env import get_auth_url, get_proxy_name, get_service_base_path
+from utils.authorization import ApplicationRestrictedPermission as AppResPerm, make_permissions_attribute, \
+    app_res_full_access
 from utils.factories import make_apigee_service, make_app_restricted_app, make_apigee_product
 from utils.immunisation_api import ImmunisationApi
 from utils.resource import create_an_imms_obj
 
 
-class TestAuthorization(unittest.TestCase):
+class TestApplicationRestrictedAuthorization(unittest.TestCase):
     apigee_service: ApigeeService
     product: ApigeeProduct
-
-    # a list of ImmunizationApi for each authentication type
-    imms_apis: List[ImmunisationApi]
-    # a list of all apps that was created, so we can delete them at the end
-    apps: List[ApigeeApp]
-    # an ImmunisationApi with default auth-type: ApplicationRestricted
-    default_imms_api: ImmunisationApi
+    app: ApigeeApp
+    imms_api: ImmunisationApi
 
     @classmethod
     def setUpClass(cls):
@@ -28,15 +25,9 @@ class TestAuthorization(unittest.TestCase):
 
         product_data = ApigeeProduct(name=str(uuid.uuid4()),
                                      displayName=display_name,
-                                     # we only use one single product for all auth types
-                                     # TODO(Cis2): add scopes for Cis2
-                                     # TODO(NhsLogin): add scopes for NhsLogin
                                      scopes=[f"urn:nhsd:apim:app:level3:immunisation-fhir-api"])
         cls.product = make_apigee_product(cls.apigee_service, product_data)
         cls.apigee_service.add_proxy_to_product(product_name=cls.product.name, proxy_name=get_proxy_name())
-
-        cls.apps = []
-        cls.imms_apis = []
 
         def make_app_data() -> ApigeeApp:
             _app = ApigeeApp(name=str(uuid.uuid4()))
@@ -44,40 +35,42 @@ class TestAuthorization(unittest.TestCase):
             _app.add_product(cls.product.name)
             return _app
 
-        # ApplicationRestricted
         app_data = make_app_data()
-        app_restricted_app, app_res_cfg = make_app_restricted_app(cls.apigee_service, app_data)
-        cls.apps.append(app_restricted_app)
+        cls.app, app_res_cfg = make_app_restricted_app(cls.apigee_service, app_data)
 
         app_res_auth = AppRestrictedAuthentication(get_auth_url(), app_res_cfg)
         base_url = get_service_base_path()
 
-        cls.default_imms_api = ImmunisationApi(base_url, app_res_auth)
-        cls.imms_apis.append(cls.default_imms_api)
+        cls.imms_api = ImmunisationApi(base_url, app_res_auth)
 
-        # Cis2
-        # TODO(Cis2) create an app for Cis2 and append it to the cls.apps,
-        #  then create ImmunisationApi and append it to cls.imms_apis
-
-        # NhsLogin
-        # TODO(NhsLogin) create an app for NhsLogin and append it to the cls.apps,
-        #  then create ImmunisationApi and append it to cls.imms_apis
+    def setUp(self):
+        # modify app to full access before each test so, we can arrange the test
+        self.modify_permissions(set())
 
     @classmethod
     def tearDownClass(cls):
-        for app in cls.apps:
-            cls.apigee_service.delete_application(app.name)
+        cls.apigee_service.delete_application(cls.app.name)
         cls.apigee_service.delete_product(cls.product.name)
 
-    def test_create_imms(self):
-        """it should create a FHIR Immunization resource"""
-        # Given
+    def modify_permissions(self, permissions: Set[AppResPerm]):
+        k, v = make_permissions_attribute(permissions)
+        self.apigee_service.create_app_attribute(self.app.name, k, v)
+
+    def test_create_imms_authorised(self):
+        """it should create Immunization if app has immunization:create permission"""
+        self.modify_permissions({AppResPerm.CREATE})
+
         imms = create_an_imms_obj()
+        result = self.imms_api.create_immunization(imms)
 
-        # When
-        result = self.default_imms_api.create_immunization(imms)
-
-        # Then
         self.assertEqual(result.status_code, 201, result.text)
-        self.assertEqual(result.text, "")
-        self.assertTrue("Location" in result.headers)
+
+    def test_create_imms_unauthorised(self):
+        """it should not create Immunization if app doesn't immunization:create permission"""
+        perm = app_res_full_access(exclude={AppResPerm.CREATE})
+        self.modify_permissions(perm)
+
+        imms = create_an_imms_obj()
+        result = self.imms_api.create_immunization(imms)
+
+        self.assertEqual(result.status_code, 403, result.text)
--- e2e/utils/factories.py
+++ e2e/utils/factories.py
@@ -1,12 +1,15 @@
 import os
 import os
 import uuid
+from typing import Set
 
 from lib.apigee import ApigeeService, ApigeeConfig, ApigeeApp, ApigeeProduct
 from lib.authentication import AppRestrictedCredentials, AppRestrictedAuthentication
 from lib.env import get_apigee_access_token, get_auth_url, get_apigee_username, get_apigee_env, \
     get_default_app_restricted_credentials, get_proxy_name
 from lib.jwks import JwksData
+from utils.authorization import ApplicationRestrictedPermission as AppResPerms, app_res_full_access, \
+    make_permissions_attribute
 
 JWKS_PATH = f"{os.getcwd()}/.well-known"
 PRIVATE_KEY_PATH = f"{os.getcwd()}/.keys"
@@ -35,7 +38,8 @@ def make_apigee_product(apigee: ApigeeService = None, product: ApigeeProduct = N
 
 
 def make_app_restricted_app(apigee: ApigeeService = None,
-                            app: ApigeeApp = None) -> (ApigeeApp, AppRestrictedCredentials):
+                            app: ApigeeApp = None,
+                            permissions: Set[AppResPerms] = None) -> (ApigeeApp, AppRestrictedCredentials):
     if not apigee:
         apigee = make_apigee_service()
 
@@ -45,22 +49,25 @@ def make_app_restricted_app(apigee: ApigeeService = None,
         stored_app = ApigeeApp(name="default-app")
         return stored_app, cred
     else:
-        # we use this prefix for file names. This way we don't create a separate file for each jwks and override the
-        #  previous one
+        # we use this prefix for file names. This way we don't create a separate file for each jwks
         key_id_prefix = get_proxy_name()
+        # NOTE: adding uuid is important. identity-service caches the key_id so,
+        #  this way we know it'll be invalidated each time we create a new jwks
         key_id = f"{key_id_prefix}-{str(uuid.uuid4())}"
 
         jwks_data = JwksData(key_id)
         jwks_url = jwks_data.get_jwks_url(base_url="https://api.service.nhs.uk/mock-jwks")
-
         app.add_attribute("jwks-resource-url", jwks_url)
+
+        if permissions := permissions or app_res_full_access():
+            k, v = make_permissions_attribute(permissions)
+            app.add_attribute(k, v)
+
         resp = apigee.create_application(app)
         stored_app = ApigeeApp.from_dict(resp)
 
-        print(jwks_url)
+        credentials = AppRestrictedCredentials(client_id=stored_app.get_client_id(),
+                                               kid=key_id,
+                                               private_key_content=jwks_data.private_key)
 
-        config = AppRestrictedCredentials(client_id=stored_app.get_client_id(),
-                                          kid=key_id,
-                                          private_key_content=jwks_data.private_key)
-
-        return stored_app, config
+        return stored_app, credentials
