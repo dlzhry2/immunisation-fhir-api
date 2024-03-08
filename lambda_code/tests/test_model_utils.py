dc887e07dd54a85192cbd62288f7a0b66870e43f
nathanwall <me+github@nathanwall.co.uk>
2024-03-08 11:12:29 +0000

    Add unit tests for get_nhs_number_verification_status_code function

--- lambda_code/tests/test_model_utils.py
+++ lambda_code/tests/test_model_utils.py
@@ -1,11 +1,17 @@
 """Tests for the utils module"""
 
 import unittest
-from models.utils.generic_utils import nhs_number_mod11_check
+from copy import deepcopy
+from models.utils.generic_utils import nhs_number_mod11_check, get_nhs_number_verification_status_code
+from .utils.generic_utils import load_json_data
 
 
 class UtilsTests(unittest.TestCase):
     """Tests for models.utils.generic_utils module"""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.json_data = load_json_data("sample_covid_immunization_event.json")
 
     def test_nhs_number_mod11_check(self):
         """Test the nhs_number_mod11_check function"""
@@ -29,3 +35,20 @@ class UtilsTests(unittest.TestCase):
 
         for invalid_nhs_number in invalid_nhs_numbers:
             self.assertFalse(nhs_number_mod11_check(invalid_nhs_number))
+
+    def test_get_nhs_number_verification_status_code(self):
+        """Test the get_nhs_number_verification_status_code function"""
+        # The NHS number verification status code is 01
+        nhs_number_verification_status_code = get_nhs_number_verification_status_code(self.json_data)
+        self.assertEqual(nhs_number_verification_status_code, "01")
+
+        self.json_data["contained"][1]["identifier"][0]["extension"][0]["valueCodeableConcept"]["coding"][0][
+            "code"
+        ] = "04"
+        nhs_number_verification_status_code = get_nhs_number_verification_status_code(self.json_data)
+        self.assertEqual(nhs_number_verification_status_code, "04")
+
+        bad_json_data = deepcopy(self.json_data)
+        del bad_json_data["contained"][1]["identifier"][0]["extension"][0]["valueCodeableConcept"]["coding"][0]["code"]
+        nhs_number_verification_status_code = get_nhs_number_verification_status_code(bad_json_data)
+        self.assertIsNone(nhs_number_verification_status_code)
