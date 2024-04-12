5bb04ba9f2192b7fb9977c21228cd205f8beb89e
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-04-12 18:24:52 +0100

    Modified handler for unsupported operation

--- backend/src/not_found_handler.py
+++ backend/src/not_found_handler.py
@@ -1,36 +1,76 @@
 import json
 
+ALLOWED_METHODS = ["GET", "POST", "DELETE", "PUT"]
+
 
 def not_found_handler(event, context):
-    response = {
-        "statusCode": 404,
-        "headers": {
-            "Content-Type": "application/json",
-        },
-        "body": json.dumps({
-            "resourceType": "OperationOutcome",
-            "id": "a5abca2a-4eda-41da-b2cc-95d48c6b791d",
-            "meta": {
-                "profile": [
-                    "https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"
-                ]
+
+    if event.get("httpMethod") not in ALLOWED_METHODS:
+        response = {
+            "statusCode": 405,
+            "headers": {
+                "Content-Type": "application/json",
             },
-            "issue": [
+            "body": json.dumps(
                 {
-                    "severity": "error",
-                    "code": "not-found",
-                    "details": {
-                        "coding": [
-                            {
-                                "system": "https://fhir.nhs.uk/Codesystem/http-error-codes",
-                                "code": "NOT_FOUND"
-                            }
+                    "resourceType": "OperationOutcome",
+                    "id": "a5abca2a-4eda-41da-b2cc-95d48c6b791d",
+                    "meta": {
+                        "profile": [
+                            "https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"
                         ]
                     },
-                    "diagnostics": "The requested resource was not found."
+                    "issue": [
+                        {
+                            "severity": "error",
+                            "code": "not-supported",
+                            "details": {
+                                "coding": [
+                                    {
+                                        "system": "https://fhir.nhs.uk/Codesystem/http-error-codes",
+                                        "code": "NOT_SUPPORTED",
+                                    }
+                                ]
+                            },
+                            "diagnostics": "Method Not Allowed",
+                        }
+                    ],
                 }
-            ]
-        })
-    }
+            ),
+        }
+        return response
 
-    return response
+    else:
+        response = {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": json.dumps(
+                {
+                    "resourceType": "OperationOutcome",
+                    "id": "a5abca2a-4eda-41da-b2cc-95d48c6b791d",
+                    "meta": {
+                        "profile": [
+                            "https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"
+                        ]
+                    },
+                    "issue": [
+                        {
+                            "severity": "error",
+                            "code": "not-found",
+                            "details": {
+                                "coding": [
+                                    {
+                                        "system": "https://fhir.nhs.uk/Codesystem/http-error-codes",
+                                        "code": "NOT_FOUND",
+                                    }
+                                ]
+                            },
+                            "diagnostics": "The requested resource was not found.",
+                        }
+                    ],
+                }
+            ),
+        }
+        return response
