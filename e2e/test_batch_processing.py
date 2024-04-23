cefbcc20fba93d53c80e211ad015d29daef189e0
amitkr-22 <amit.kumar22@nhs.net>
2024-04-23 18:22:13 +0100

    commented upload to S3 tests

--- e2e/test_batch_processing.py
+++ e2e/test_batch_processing.py
@@ -51,31 +51,31 @@ class TestBatchProcessing(unittest.TestCase):
     ctl_key = None
     report_key = None
 
-    @classmethod
-    def setUpClass(cls):
-        name = str(uuid.uuid4())
-        cls.dat_key = f"{name}.dat"
-        cls.ctl_key = f"{name}.ctl"
-        cls.report_key = cls.dat_key
+    # @classmethod
+    # def setUpClass(cls):
+    #     name = str(uuid.uuid4())
+    #     cls.dat_key = f"{name}.dat"
+    #     cls.ctl_key = f"{name}.ctl"
+    #     cls.report_key = cls.dat_key
 
-        session = boto3.Session(profile_name="apim-dev")
-        aws_conf = Config(region_name="eu-west-2")
-        s3_client = session.client("s3", config=aws_conf)
-        cls.s3_client = s3_client
+    #     session = boto3.Session(profile_name="apim-dev")
+    #     aws_conf = Config(region_name="eu-west-2")
+    #     s3_client = session.client("s3", config=aws_conf)
+    #     cls.s3_client = s3_client
 
-        source_bucket = get_s3_source_name()
-        date_file = cls.make_batch_file()
-        # date_file.upload_to_s3(cls.s3_client, source_bucket, cls.dat_key)
-        date_file.upload_to_s3(cls.s3_client, source_bucket)
-        ctl_file = make_ctl_file()
-        # ctl_file.upload_to_s3(cls.s3_client, source_bucket, cls.ctl_key)
-        ctl_file.upload_to_s3(cls.s3_client, source_bucket)
+    #     source_bucket = get_s3_source_name()
+    #     date_file = cls.make_batch_file()
+    #     # date_file.upload_to_s3(cls.s3_client, source_bucket, cls.dat_key)
+    #     date_file.upload_to_s3(cls.s3_client, source_bucket)
+    #     ctl_file = make_ctl_file()
+    #     # ctl_file.upload_to_s3(cls.s3_client, source_bucket, cls.ctl_key)
+    #     ctl_file.upload_to_s3(cls.s3_client, source_bucket)
 
-        logger.debug("waiting for the batch processing to finish")
-        # wait for the event rule to start the task and then wait for the task to finish
-        sleep(5)
-        ecs_client = session.client('ecs', config=aws_conf)
-        _wait_for_batch_processing(ecs_client, get_cluster_name())
+    #     logger.debug("waiting for the batch processing to finish")
+    #     # wait for the event rule to start the task and then wait for the task to finish
+    #     sleep(5)
+    #     ecs_client = session.client('ecs', config=aws_conf)
+    #     _wait_for_batch_processing(ecs_client, get_cluster_name())
 
     @staticmethod
     def make_batch_file() -> BatchFile:
