8ef61f3b1400f675b8e79a50441aa5854a667f70
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-05-02 20:38:36 +0100

    Fixed lint changes

--- e2e/test_s3_vaccine_locations.py
+++ e2e/test_s3_vaccine_locations.py
@@ -2,8 +2,8 @@ import boto3
 import unittest
 from utils.batch import get_s3_source_name
 
-class TestS3Batch(unittest.TestCase):
 
+class TestS3Batch(unittest.TestCase):
     def test_s3_folder_structure(self):
         source_bucket = get_s3_source_name()
         print(f"source bucket: {source_bucket}")
@@ -11,7 +11,9 @@ class TestS3Batch(unittest.TestCase):
         expected_folders = ["COVID19_POC/", "COVID19/", "FLU_POC/", "FLU/"]
         objects = s3.list_objects_v2(Bucket=source_bucket)
         actual_folders = [
-            obj["Key"] for obj in objects.get("Contents", []) if obj["Key"].endswith("/")
+            obj["Key"]
+            for obj in objects.get("Contents", [])
+            if obj["Key"].endswith("/")
         ]
         assert expected_folders == actual_folders
 
