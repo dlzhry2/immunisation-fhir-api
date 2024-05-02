c9fcaee78d1561fc600ff8f7ddc52ce45364cb72
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-05-02 17:54:55 +0100

    End to end test case for S3 vaccine type locations

---
+++ e2e/test_s3_vaccine_locations.py
@@ -0,0 +1,15 @@
+import os
+import boto3
+import pytest
+from utils.batch import get_s3_source_name
+
+
+def test_s3_folder_structure():
+    source_bucket = get_s3_source_name()
+    s3 = boto3.client("s3")
+    expected_folders = ["COVID19_POC/", "COVID19/", "FLU_POC/", "FLU/"]
+    objects = s3.list_objects_v2(Bucket=source_bucket)
+    actual_folders = [
+        obj["Key"] for obj in objects.get("Contents", []) if obj["Key"].endswith("/")
+    ]
+    assert expected_folders == actual_folders
