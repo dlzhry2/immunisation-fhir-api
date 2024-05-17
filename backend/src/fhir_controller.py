1f92e7726a70cf2cd9f68f55bf4eed92fb7d70b7
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-05-17 13:00:48 +0100

    AMB 2013 - Added the condition to remove the vaccine status not done in the response

--- backend/src/fhir_controller.py
+++ backend/src/fhir_controller.py
@@ -181,6 +181,7 @@ class FhirController:
             search_params.date_from,
             search_params.date_to,
         )
+        print(f"result :{result}")
 
         if "diagnostics" in result: 
            exp_error = create_operation_outcome(
@@ -193,6 +194,9 @@ class FhirController:
         # Workaround for fhir.resources JSON removing the empty "entry" list.
         result_json_dict: dict = json.loads(result.json())
         if "entry" in result_json_dict:
+            result_json_dict['entry'] = [result_json_dict for entry in
+                                         result_json_dict['entry']
+                                         if entry['resource'].get('status') != 'not-done']
             total_count = sum(1 for entry in result_json_dict["entry"] if entry.get("search", {}).get("mode") == "match")
             result_json_dict["total"] = total_count
         if "entry" not in result_json_dict:
