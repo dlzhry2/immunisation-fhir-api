9e5057aaebe4a4a42b5b5af466cb7f7a70e611ac
Akash <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2024-05-22 18:03:26 +0100

    AMB 2053 - Added the testcases for update_imms_with_duplicated_identifier_returns_error

--- backend/tests/test_update_imms.py
+++ backend/tests/test_update_imms.py
@@ -48,3 +48,13 @@ class TestUpdateImmunizations(unittest.TestCase):
     def test_update_imms_with_duplicated_identifier_returns_error(self):
         """Should return an IdentifierDuplication error"""
         # TODO: BUG Implement this test
+        lambda_event = {"pathParameters": {"id": "an-id"}}
+        error_msg = {'statusCode': 422, 'headers': {'Content-Type': 'application/fhir+json'}, 'body': '{"resourceType": "OperationOutcome", "id": "5c132d8a-a928-4e0e-8792-0c6456e625c2", "meta": {"profile": ["https://simplifier.net/guide/UKCoreDevelopment2/ProfileUKCore-OperationOutcome"]}, "issue": [{"severity": "error", "code": "exception", "details": {"coding": [{"system": "https://fhir.nhs.uk/Codesystem/http-error-codes","code": "DUPLICATE"}]}, "diagnostics": "The provided identifier: id-id is duplicated"}]}'}
+        self.controller.update_immunization.return_value = error_msg
+
+        act_res = update_imms(lambda_event, self.controller)
+       
+        # Then
+        self.controller.update_immunization.assert_called_once_with(lambda_event)
+        self.assertEqual(act_res["statusCode"], 422)
+        self.assertDictEqual(act_res, error_msg)
