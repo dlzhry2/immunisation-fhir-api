4a65e26a12c2a2fb0da0439f22a413d7188dd9d4
AlexandraBenson <alexandra.benson3@nhs.net>
2024-06-27 22:15:17 +0100

    Add Urls class to constants

--- backend/src/constants.py
+++ backend/src/constants.py
@@ -1,2 +1,19 @@
-valid_nhs_number = "1345678940"  # Valid for pre, FHIR and post validators
-address_unknown_postcode = "ZZ99 3WZ"
+"""Constants"""
+
+# Constants for use within the test
+VALID_NHS_NUMBER = "1345678940"  # Valid for pre, FHIR and post validators
+ADDRESS_UNKNOWN_POSTCODE = "ZZ99 3WZ"
+
+
+class Urls:
+    """Urls which are expected to be used within the FHIR Immunization Resource json data"""
+
+    nhs_number = "https://fhir.nhs.uk/Id/nhs-number"
+    vaccination_procedure = "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-VaccinationProcedure"
+    snomed = "http://snomed.info/sct"
+    nhs_number_verification_status_structure_definition = (
+        "https://fhir.hl7.org.uk/StructureDefinition/Extension-UKCore-NHSNumberVerificationStatus"
+    )
+    nhs_number_verification_status_code_system = (
+        "https://fhir.hl7.org.uk/CodeSystem/UKCore-NHSNumberVerificationStatusEngland"
+    )
