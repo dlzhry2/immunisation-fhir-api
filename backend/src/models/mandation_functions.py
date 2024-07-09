45f718015c23690a326ada06ac4ec004a13b07a8
AlexandraBenson <alexandra.benson3@nhs.net>
2024-07-09 20:30:16 +0100

    remove unnecessary code

--- backend/src/models/mandation_functions.py
+++ backend/src/models/mandation_functions.py
@@ -15,8 +15,6 @@ class MandationRules:
     mandatory = "mandatory"
     required = "required"
     optional = "optional"
-    mandatory_when_practitioner_identifier_value_present = "mandatory_when_practitioner_identifier_value_present"
-    mandatory_when_primary_source_is_false = "mandatory_when_primary_source_is_false"
 
 
 class MandationFunctions:
@@ -41,24 +39,3 @@ class MandationFunctions:
 
     def optional(self, field_value, field_location: str) -> None:
         """Allows all field values to pass"""
-
-    def mandatory_when_practitioner_identifier_value_present(self, field_value, field_location: str) -> None:
-        """Raises MandatoryError if practitioner_identifier_field is present and given field value is None"""
-
-        practitioner_identifier_value = obtain_field_value(self.imms, FieldNames.practitioner_identifier_value)
-        practitioner_identifier_value_field_location = getattr(FieldLocations, FieldNames.practitioner_identifier_value)
-
-        if practitioner_identifier_value is not None and field_value is None:
-            raise MandatoryError(
-                f"{field_location} is mandatory when {practitioner_identifier_value_field_location} is"
-                + f" present and vaccination type is {self.vaccine_type}"
-            )
-
-    def mandatory_when_primary_source_is_false(self, field_value, field_location: str) -> None:
-        """Raises MandatoryError if primary_source is false and given field value is None"""
-
-        primary_source = obtain_field_value(self.imms, FieldNames.primary_source)
-        primary_source_field_location = getattr(FieldLocations, FieldNames.primary_source)
-
-        if primary_source is False and field_value is None:
-            raise MandatoryError(f"{field_location} is mandatory when {primary_source_field_location} is false")
