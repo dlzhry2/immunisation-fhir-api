6fc23cc1e1cb03c02786aaed9c8abe9fc23c857d
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-08-01 14:31:36 +0100

    fixed test

--- backend/tests/test_fhir_repository.py
+++ backend/tests/test_fhir_repository.py
@@ -502,7 +502,7 @@ class TestFindImmunizations(unittest.TestCase):
 
         is_ = Attr("DeletedAt").not_exists() | Attr("DeletedAt").eq("reinstated")
 
-        # Whenn
+        # When
         _ = self.repository.find_immunizations("an-id", [VaccineTypes.covid_19])
 
         # Then
