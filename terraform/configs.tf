d9c44c69671d4ecc06a899cb131c94ce75c044e3
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-10-15 17:13:49 +0100

    few changes

--- terraform/configs.tf
+++ terraform/configs.tf
@@ -3,4 +3,5 @@ locals {
     is_temp = length(regexall("[a-z]{2,4}-?[0-9]+", local.environment)) > 0
     account_id = local.environment == "prod" ? 232116723729 : 603871901111
     local_account_id = local.environment == "prod" ? 345594581768 : 345594581768
+    local_config = local.environment == "prod" ? "prod" : "internal-dev"
 }
