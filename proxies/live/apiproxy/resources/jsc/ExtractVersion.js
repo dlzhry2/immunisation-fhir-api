eee26c46e7bef382ee2c4ee86af649d5b6510090
amitkr-22 <amit.kumar22@nhs.net>
2024-10-30 14:05:30 +0000

    changes related to invalid accept header

--- proxies/live/apiproxy/resources/jsc/ExtractVersion.js
+++ proxies/live/apiproxy/resources/jsc/ExtractVersion.js
@@ -1,10 +1,19 @@
 // extract version from Accept header
 function extractVersion() {
     var acceptHeader = context.getVariable("request.header.Accept");
-    var versionPattern = /application\/fhir\+json;\s*version=(\d+)/i;
-    var match = acceptHeader.match(versionPattern);
-    var version = match ? match[1] : "1"; // Default to version 1 if not provided
-    context.setVariable("version", version);
+    if (acceptHeader) {
+        var versionPattern = /application\/fhir\+json;\s*version=(\d+)/i; // 'i' flag for case-insensitivity
+        var validPattern = /^application\/fhir\+json(;|;\s*version=\d+)?$/i; // Valid patterns: with or without version
+        if (validPattern.test(acceptHeader)) {
+            var match = acceptHeader.match(versionPattern);
+            var version = match ? match[1] : "1"; // Default to version 1 if not provided
+            context.setVariable("version", version);
+        } else {
+            context.setVariable("invalidAcceptHeader", true);
+        }
+    } else {
+        context.setVariable("version", "1"); // Default to version 1 if Accept header is missing
+    }
 }
 
 extractVersion();
