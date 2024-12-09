ebb5220fdaa9b04c30c2d05ba031874f207782be
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-12-09 14:33:30 +0000

    Few changes

--- terraform_aws_backup/aws-backup-source/modules/aws_config/backup_plan.tf
+++ terraform_aws_backup/aws-backup-source/modules/aws_config/backup_plan.tf
@@ -75,7 +75,7 @@ resource "aws_backup_selection" "dynamodb" {
   count        = var.backup_plan_config_dynamodb.enable ? 1 : 0
   iam_role_arn = aws_iam_role.backup.arn
   name         = "${local.resource_name_prefix}-dynamodb-selection"
-  plan_id      = aws_backup_plan.dynamodb.id
+  plan_id      = aws_backup_plan.dynamodb[0].id
 
   selection_tag {
     key   = var.backup_plan_config_dynamodb.selection_tag
