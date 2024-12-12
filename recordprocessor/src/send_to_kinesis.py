2414131e5be66bf4d79605b22ebafde509e344d4
AlexandraBenson <alexandra.benson3@nhs.net>
2024-12-12 15:59:38 +0000

    Add logging to terraform and create logging decorator

--- recordprocessor/src/send_to_kinesis.py
+++ recordprocessor/src/send_to_kinesis.py
@@ -8,25 +8,13 @@ from s3_clients import kinesis_client
 logger = logging.getLogger()
 
 
-def send_to_kinesis(
-    supplier: str, message_body: dict, stream_name: str, stream_arn: str
-) -> bool:
+def send_to_kinesis(supplier: str, message_body: dict, stream_name: str, stream_arn: str) -> bool:
     """Send a message to the specified Kinesis stream. Returns a boolean indicating whether the send was successful."""
     try:
 
         data = json.dumps(message_body, ensure_ascii=False)
-        resp = kinesis_client.put_record(
-            StreamName=stream_name,
-            StreamARN=stream_arn,
-            Data=data,
-            PartitionKey=supplier,
-        )
-        logger.info(
-            "Message sent to Kinesis stream: %s for supplier: %s with resp: %s",
-            stream_name,
-            supplier,
-            resp,
-        )
+        resp = kinesis_client.put_record(StreamName=stream_name, StreamARN=stream_arn, Data=data, PartitionKey=supplier)
+        logger.info("Message sent to Kinesis stream: %s for supplier: %s with resp: %s", stream_name, supplier, resp)
         return True
     except ClientError as error:
         logger.error("Error sending message to Kinesis: %s", error)
