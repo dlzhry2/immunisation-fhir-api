52f81bfb143cee1cd8d758393b6a400be9a3370a
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-12-16 12:33:12 +0000

    Output tf updated

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "service_domain_name" {
   value = local.service_domain_name
 }
-# output "dynamodb_table_name" {
-#   value = aws_dynamodb_table.events-dynamodb-table.name
-# }
-# output "imms_delta_table_name" {
-#   value = aws_dynamodb_table.delta-dynamodb-table.name
-# }
+output "dynamodb_table_name" {
+  value = data.aws_dynamodb_table.events-dynamodb-table.name
+}
+output "imms_delta_table_name" {
+  value = data.aws_dynamodb_table.delta-dynamodb-table.name
+}
 output "aws_sqs_queue_name" {
   value = aws_sqs_queue.dlq.name
 }
