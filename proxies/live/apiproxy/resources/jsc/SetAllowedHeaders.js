ccbd70fa89ad77178a43e56dff5feaba575a8db7
CLJ2006 <clarissa.john1@nhs.net>
2025-01-02 11:40:57 +0000

    updates

---
+++ proxies/live/apiproxy/resources/jsc/SetAllowedHeaders.js
@@ -0,0 +1,23 @@
+// Define the allowed headers 
+var allowedHeaders = [
+    'Date',
+    'Content-Type',
+    'Content-Length',
+    'Connection',
+    'X-Correlation-ID',
+    'X-Request-ID',
+    'Accept',
+    'Strict-Transport-Security'
+];
+
+var responseHeaders = context.getVariable('response.headers.names') || [];
+
+var headersToRemove = responseHeaders.filter(function(header) {
+    return allowedHeaders.indexOf(header) === -1;
+});
+
+context.setVariable('debug.headersToRemove', headersToRemove.join(","));
+
+context.setVariable('headersToRemove', headersToRemove.join(","));
+
+
