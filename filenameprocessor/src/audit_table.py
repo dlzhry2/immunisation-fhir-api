48dbd132b47194fe14916061df0a14eb9bd5e0e9
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2025-01-13 13:36:30 +0000

    Audit table check

--- filenameprocessor/src/audit_table.py
+++ filenameprocessor/src/audit_table.py
@@ -6,8 +6,13 @@ from clients import dynamodb_client, dynamodb_resource, logger
 from errors import DuplicateFileError, UnhandledAuditTableError
 
 
-def add_to_audit_table(message_id: str,
-                       file_key: str, created_at_formatted_str: str, queue_name: str, process_status: str) -> None:
+def add_to_audit_table(
+    message_id: str,
+    file_key: str,
+    created_at_formatted_str: str,
+    queue_name: str,
+    process_status: str,
+) -> None:
     """
     Adds the filename to the audit table.
     Raises an error if the file is a duplicate (after adding it to the audit table).
@@ -22,16 +27,16 @@ def add_to_audit_table(message_id: str,
             IndexName=file_name_gsi, KeyConditionExpression=Key("filename").eq(file_key)
         )
         duplicate_exists = bool(file_name_response.get("Items"))
-        
+
         # Check for files un der processing for Supplier_Vaccine combination, if yes queue file for processing
         if not duplicate_exists:
             queue_response = dynamodb_resource.Table(table_name).query(
-                IndexName=queue_name_gsi, KeyConditionExpression=Key("queue_name").eq(queue_name) &
-                                                                Key("status").eq(process_status)
+                IndexName=queue_name_gsi,
+                KeyConditionExpression=Key("queue_name").eq(queue_name)
+                & Key("status").eq(process_status),
             )
             if queue_response["Items"]:
                 process_status = "Queued"
-        
 
         # Add to the audit table (regardless of whether it is a duplicate)
         dynamodb_client.put_item(
@@ -40,12 +45,22 @@ def add_to_audit_table(message_id: str,
                 "message_id": {"S": message_id},
                 "filename": {"S": file_key},
                 "queue_name": {"S": queue_name},
-                "status": {"S": "Not processed - duplicate" if duplicate_exists else process_status},
+                "status": {
+                    "S": (
+                        "Not processed - duplicate"
+                        if duplicate_exists
+                        else process_status
+                    )
+                },
                 "timestamp": {"S": created_at_formatted_str},
             },
             ConditionExpression="attribute_not_exists(message_id)",  # Prevents accidental overwrites
         )
-        logger.info("%s file, with message id %s, successfully added to audit table", file_key, message_id)
+        logger.info(
+            "%s file, with message id %s, successfully added to audit table",
+            file_key,
+            message_id,
+        )
 
     except Exception as error:  # pylint: disable = broad-exception-caught
         error_message = f"Error adding {file_key} to the audit table"
@@ -54,5 +69,9 @@ def add_to_audit_table(message_id: str,
 
     # If a duplicte exists, raise an exception
     if duplicate_exists:
-        logger.error("%s file duplicate added to s3 at the following time: %s", file_key, created_at_formatted_str)
+        logger.error(
+            "%s file duplicate added to s3 at the following time: %s",
+            file_key,
+            created_at_formatted_str,
+        )
         raise DuplicateFileError(f"Duplicate file: {file_key}")
