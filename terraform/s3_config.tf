ae4411a48c0823ad89ec27e85ed6ded02c2d8ebb
ASubaran <akra2@hscic.gov.uk>
2025-01-16 10:59:13 +0000

    Removed encryption for source bucket

--- terraform/s3_config.tf
+++ terraform/s3_config.tf
@@ -19,16 +19,16 @@ resource "aws_s3_bucket_policy" "batch_data_source_bucket_policy" {
    policy = data.aws_iam_policy_document.batch_data_source_bucket_policy.json
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_batch_source_encryption" {
-  bucket = aws_s3_bucket.batch_data_source_bucket.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "s3_batch_source_encryption" {
+#   bucket = aws_s3_bucket.batch_data_source_bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = data.aws_kms_key.existing_s3_encryption_key.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = data.aws_kms_key.existing_s3_encryption_key.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
 
 resource "aws_s3_bucket_versioning" "source_versioning" {
   bucket = aws_s3_bucket.batch_data_source_bucket.id
