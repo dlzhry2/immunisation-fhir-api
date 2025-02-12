271ac87fbcdbd5279c4775c8ed0c1b7200c328fa
AlexandraBenson <alexandra.benson3@nhs.net>
2025-02-12 12:06:17 +0000

    Add unit tests for upsert_audit_table

--- filenameprocessor/tests/test_audit_table.py
+++ filenameprocessor/tests/test_audit_table.py
@@ -2,20 +2,25 @@
 
 from unittest import TestCase
 from unittest.mock import patch
+from copy import deepcopy
+import json
 from boto3 import client as boto3_client
 from moto import mock_dynamodb
 
 from tests.utils_for_tests.mock_environment_variables import MOCK_ENVIRONMENT_DICT
 from tests.utils_for_tests.generic_setup_and_teardown import GenericSetUp, GenericTearDown
+from tests.utils_for_tests.values_for_tests import MockFileDetails, MockAuditTableEntry, AuditTableEntry
 
 # Ensure environment variables are mocked before importing from src files
 with patch.dict("os.environ", MOCK_ENVIRONMENT_DICT):
-    from constants import AUDIT_TABLE_NAME
-    from audit_table import upsert_audit_table
-    from errors import UnhandledAuditTableError
+    from constants import AUDIT_TABLE_NAME, AuditTableKeys, FileStatus
+    from audit_table import upsert_audit_table, ensure_file_is_not_a_duplicate, get_next_queued_file_details
+    from errors import UnhandledAuditTableError, DuplicateFileError
     from clients import REGION_NAME
 
 dynamodb_client = boto3_client("dynamodb", region_name=REGION_NAME)
+
+FILE_DETAILS = MockFileDetails.rsv_ravs
 
 
 @mock_dynamodb
@@ -35,6 +40,181 @@ class TestAuditTable(TestCase):
     def get_table_items():
         """Return all items in the audit table"""
         return dynamodb_client.scan(TableName=AUDIT_TABLE_NAME).get("Items", [])
+
+    @staticmethod
+    def add_entry_to_table(entry: MockAuditTableEntry):
+        """Add an entry to the audit table"""
+        dynamodb_client.put_item(
+            TableName=AUDIT_TABLE_NAME, Item={k: {"S": v} for k, v in entry.audit_table_entry.items()}
+        )
+
+    def assert_table_entry(self, file_details: AuditTableEntry, expected_status: FileStatus):
+        """Assert that the file details are in the audit table"""
+        table_entry = dynamodb_client.get_item(
+            TableName=AUDIT_TABLE_NAME, Key={AuditTableKeys.MESSAGE_ID: {"S": file_details.message_id}}
+        ).get("Item")
+        expected_audit_table_entry = {**file_details.audit_table_entry, "status": expected_status}
+        self.assertEqual(table_entry, {k: {"S": v} for k, v in expected_audit_table_entry.items()})
+
+    def test_get_next_queued_file_details(self):
+        """Test that the get_next_queued_file_details function returns the correct file details"""
+        # NOTE: Throughout this test the assertions will be checking for the next queued RAVS_RSV file.
+        queue_to_check = "RAVS_RSV"
+
+        # Test case 1: no files in audit table
+        self.assertIsNone(get_next_queued_file_details(queue_to_check))
+
+        # Test case 2: files in audit table, but none of the files are in the RAVS_RSV queue
+        self.add_entry_to_table(MockAuditTableEntry.emis_flu_1_queued)  # different queue
+        self.add_entry_to_table(MockAuditTableEntry.emis_rsv_1_queued)  # different queue
+        self.add_entry_to_table(MockAuditTableEntry.ravs_flu_1_queued)  # different queue
+        self.add_entry_to_table(MockAuditTableEntry.ravs_rsv_1_processed)  # same queue but already processed
+        self.assertIsNone(get_next_queued_file_details(queue_to_check))
+
+        # Test case 3: one queued file in the ravs_rsv queue
+        self.add_entry_to_table(MockAuditTableEntry.ravs_rsv_2_queued)
+        self.assertEqual(
+            get_next_queued_file_details(queue_to_check), MockAuditTableEntry.ravs_rsv_2_queued.audit_table_entry
+        )
+
+        # Test case 4: multiple queued files in the RAVS_RSV queue
+        # Note that ravs_rsv files 3 and 4 have later timestamps than file 2, so file 2 remains the first in the queue
+        self.add_entry_to_table(MockAuditTableEntry.ravs_rsv_3_queued)
+        self.add_entry_to_table(MockAuditTableEntry.ravs_rsv_4_queued)
+        self.assertEqual(
+            get_next_queued_file_details(queue_to_check), MockAuditTableEntry.ravs_rsv_2_queued.audit_table_entry
+        )
+
+    def test_ensure_file_is_not_a_duplicate(self):
+        """
+        Tests that ensure_file_is_not_a_duplicate raises a DuplicateFile Error if and only if the file is a
+        duplicate in the audit table.
+        """
+        # Test case 1: file is not a duplicate (and so is not currently in the audit table)
+        self.assertIsNone(
+            ensure_file_is_not_a_duplicate(FILE_DETAILS.file_key, FILE_DETAILS.created_at_formatted_string)
+        )
+
+        # Add the file to the audit table
+        dynamodb_client.put_item(
+            TableName=AUDIT_TABLE_NAME,
+            Item={
+                AuditTableKeys.MESSAGE_ID: {"S": FILE_DETAILS.message_id},
+                AuditTableKeys.FILENAME: {"S": FILE_DETAILS.file_key},
+                AuditTableKeys.TIMESTAMP: {"S": FILE_DETAILS.created_at_formatted_string},
+            },
+        )
+
+        # Test case 2: file is a duplicate (duplicate file_key, unique creation time)
+        with self.assertRaises(DuplicateFileError):
+            ensure_file_is_not_a_duplicate(
+                FILE_DETAILS.file_key, FILE_DETAILS.created_at_formatted_string.replace("2024", "2025")
+            )
+
+    def test_upsert_audit_table_2(self):
+        """Test that the upsert_audit_table function works as expected for the following:
+        * Unique files: file is added to the audit table, no error is raised
+        * Duplicate files (duplicated file name): file is added to the audit table, DuplicateFileError is raised
+        * Overwrites (duplicated message_id): file is not added to the audit table, UnhandledAuditTableError is raised
+        """
+        # Populate the table with some entries which are not in the same queue
+        self.add_entry_to_table(MockAuditTableEntry.emis_flu_1_queued)  # different queue
+        self.add_entry_to_table(MockAuditTableEntry.emis_rsv_1_queued)  # different queue
+        self.add_entry_to_table(MockAuditTableEntry.ravs_flu_1_queued)  # different queue
+
+        # Test case 1: new file with status of 'Processed'.
+        # File should be added to the audit table, with status 'Processed'. Return value should be False.
+        test_file_1 = AuditTableEntry("RAVS", "RSV", "YGM41", FileStatus.PROCESSED, file_number=1)
+
+        result = upsert_audit_table(
+            message_id=test_file_1.message_id,
+            file_key=test_file_1.filename,
+            created_at_formatted_str=test_file_1.created_at_formatted_string,
+            queue_name=test_file_1.queue_name,
+            file_status=FileStatus.PROCESSED,
+            is_existing_file=False,
+        )
+
+        self.assertFalse(result)
+        self.assert_table_entry(test_file_1, FileStatus.PROCESSED)
+
+        # Test case 2: Duplicate file with status of 'Duplicate'.
+        # Audit table status should be updated to 'Duplicate'. Return value should be False.
+        test_file_2 = AuditTableEntry("RAVS", "RSV", "YGM41", FileStatus.DUPLICATE, file_number=2)
+        test_file_2.filename = test_file_1.filename
+        test_file_2.audit_table_entry[AuditTableKeys.FILENAME] = test_file_2.filename
+
+        result = upsert_audit_table(
+            message_id=test_file_2.message_id,
+            file_key=test_file_2.filename,
+            created_at_formatted_str=test_file_2.created_at_formatted_string,
+            queue_name=test_file_2.queue_name,
+            file_status=FileStatus.DUPLICATE,
+            is_existing_file=False,
+        )
+
+        self.assertFalse(result)
+        self.assert_table_entry(test_file_2, FileStatus.DUPLICATE)
+
+        # Test case 3: new file with status of 'Processing', and no files ahead in the queue.
+        # Audit table status should be updated to 'Processing'. Return value should be False.
+        test_file_3 = AuditTableEntry("RAVS", "RSV", "YGM41", FileStatus.PROCESSING, file_number=3)
+
+        result = upsert_audit_table(
+            message_id=test_file_3.message_id,
+            file_key=test_file_3.filename,
+            created_at_formatted_str=test_file_3.created_at_formatted_string,
+            queue_name=test_file_3.queue_name,
+            file_status=FileStatus.PROCESSING,
+            is_existing_file=False,
+        )
+
+        self.assertFalse(result)
+        self.assert_table_entry(test_file_3, FileStatus.PROCESSING)
+
+        # Test case 4: new file with status of 'Processing', and files ahead in the queue.
+        # File should be added to the audit table, with status 'Queued'. Return value should be True.
+        test_file_4 = AuditTableEntry("RAVS", "RSV", "YGM41", FileStatus.PROCESSING, file_number=4)
+
+        result = upsert_audit_table(
+            message_id=test_file_4.message_id,
+            file_key=test_file_4.filename,
+            created_at_formatted_str=test_file_4.created_at_formatted_string,
+            queue_name=test_file_4.queue_name,
+            file_status=FileStatus.PROCESSING,
+            is_existing_file=False,
+        )
+
+        self.assertTrue(result)
+        self.assert_table_entry(test_file_4, FileStatus.QUEUED)
+
+        # Test case 5: existing file with status of 'Processing'.
+        # Audit table status should be updated to 'Processing'. Return value should be False.
+        result = upsert_audit_table(
+            message_id=test_file_4.message_id,
+            file_key=test_file_4.filename,
+            created_at_formatted_str=test_file_4.created_at_formatted_string,
+            queue_name=test_file_4.queue_name,
+            file_status=FileStatus.PROCESSING,
+            is_existing_file=True,
+        )
+
+        self.assertFalse(result)
+        self.assert_table_entry(test_file_4, FileStatus.PROCESSING)
+
+        # Test case 6: existing file with status of 'Processed'.
+        # Audit table status should be updated to 'Processed'. Return value should be False.
+        result = upsert_audit_table(
+            message_id=test_file_4.message_id,
+            file_key=test_file_4.filename,
+            created_at_formatted_str=test_file_4.created_at_formatted_string,
+            queue_name=test_file_4.queue_name,
+            file_status=FileStatus.PROCESSED,
+            is_existing_file=True,
+        )
+
+        self.assertFalse(result)
+        self.assert_table_entry(test_file_4, FileStatus.PROCESSING)  # TODO: ?Should be processed
 
     def test_upsert_audit_table(self):
         """Test that the upsert_audit_table function works as expected for the following:
--- filenameprocessor/tests/utils_for_tests/values_for_tests.py
+++ filenameprocessor/tests/utils_for_tests/values_for_tests.py
@@ -1,6 +1,13 @@
 """File of values which can be used for testing"""
 
+from unittest.mock import patch
 from datetime import datetime
+
+from tests.utils_for_tests.mock_environment_variables import MOCK_ENVIRONMENT_DICT
+
+with patch.dict("os.environ", MOCK_ENVIRONMENT_DICT):
+    from constants import FileStatus, AuditTableKeys
+
 
 fixed_datetime = datetime(2024, 10, 29, 12, 0, 0)
 
@@ -49,3 +56,42 @@ class MockFileDetails:
 
     rsv_ravs = FileDetails("RSV", "RAVS", "X26")
     flu_emis = FileDetails("FLU", "EMIS", "YGM41")
+
+
+class AuditTableEntry:
+    """
+    Class to create and hold values for a mock file, based on the vaccine type, supplier and ods code.
+    NOTE: Supplier and ODS code are hardcoded rather than mapped, for testing purposes.
+    NOTE: The permissions_list and permissions_config are examples of full permissions for the suppler for the
+    vaccine type.
+    """
+
+    def __init__(
+        self, supplier: str, vaccine_type: str, ods_code: str, status: str = FileStatus.QUEUED, file_number: str = 1
+    ):
+        file_date_and_time_string = f"20000101T0000000{file_number}"
+        self.message_id = f"{supplier.lower()}_{vaccine_type.lower()}_test_id_{file_number}"
+        self.filename = f"{vaccine_type}_Vaccinations_v5_{ods_code}_{file_date_and_time_string}.csv"
+        self.queue_name = f"{supplier}_{vaccine_type}"
+        self.created_at_formatted_string = f"200{file_number}0101T00000000"
+        self.status = status
+        
+        self.audit_table_entry = {
+            AuditTableKeys.MESSAGE_ID: self.message_id,
+            AuditTableKeys.FILENAME: self.filename,
+            AuditTableKeys.QUEUE_NAME: self.queue_name,
+            AuditTableKeys.STATUS: self.status,
+            AuditTableKeys.TIMESTAMP: self.created_at_formatted_string,
+        }
+
+
+class MockAuditTableEntry:
+    """Class containing mock file details for use in tests"""
+
+    ravs_rsv_1_processed = AuditTableEntry("RAVS", "RSV", "X26", FileStatus.PROCESSED, file_number=1)
+    ravs_rsv_2_queued = AuditTableEntry("RAVS", "RSV", "X26", FileStatus.QUEUED, file_number=2)
+    ravs_rsv_3_queued = AuditTableEntry("RAVS", "RSV", "X26", FileStatus.QUEUED, file_number=3)
+    ravs_rsv_4_queued = AuditTableEntry("RAVS", "RSV", "X26", FileStatus.QUEUED, file_number=4)
+    emis_flu_1_queued = AuditTableEntry("EMIS", "FLU", "YGM41")
+    emis_rsv_1_queued = AuditTableEntry("EMIS", "RSV", "YGM41")
+    ravs_flu_1_queued = AuditTableEntry("RAVS", "FLU", "X26")
