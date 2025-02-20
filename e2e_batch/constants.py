301ec33dc3b47a1f9dfb725716f7b5e15c79778a
ASubaran <akra2@hscic.gov.uk>
2025-02-20 13:17:01 +0000

    added environment variable

--- e2e_batch/constants.py
+++ e2e_batch/constants.py
@@ -1,7 +1,7 @@
 import os
 
 env_value = os.environ.get("ENV", "internal-dev")
-print(env_value)
+print(f"Environment:{env_value}")
 INPUT_BUCKET = f"immunisation-batch-{env_value}-data-sources"
 INPUT_PREFIX = ""
 ACK_BUCKET = "immunisation-batch-internal-dev-data-destinations"
