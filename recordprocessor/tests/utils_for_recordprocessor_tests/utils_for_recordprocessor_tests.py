54b66757a3f45561f8fbafff68658f3b051d544b
amitkr-22 <amit.kumar22@nhs.net>
2025-03-04 14:50:01 +0000

    updated unit tests for recordprocessor

--- recordprocessor/tests/utils_for_recordprocessor_tests/utils_for_recordprocessor_tests.py
+++ recordprocessor/tests/utils_for_recordprocessor_tests/utils_for_recordprocessor_tests.py
@@ -10,6 +10,12 @@ def convert_string_to_dict_reader(data_string: str):
     """Take a data string and convert it to a csv DictReader"""
     return DictReader(StringIO(data_string), delimiter="|")
 
+def get_csv_file_dict_reader(s3_client, bucket_name: str, file_key: str) -> DictReader:
+    """Download the file from the S3 bucket and return it as a DictReader"""
+    ack_file_csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    csv_content_string = ack_file_csv_obj["Body"].read().decode("utf-8")
+    return DictReader(StringIO(csv_content_string), delimiter="|")
+
 
 class GenericSetUp:
     """
