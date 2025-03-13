7afcdc72333b71a4e60047c7c8ec3ded95fd900b
ASubaran <akashsubaranjandharmalingam.rajarajeswari1@nhs.net>
2025-03-13 15:01:41 +0000

    added the headers

--- delta_backend/src/Converter.py
+++ delta_backend/src/Converter.py
@@ -5,6 +5,14 @@ from FHIRParser import FHIRParser
 from SchemaParser import SchemaParser
 from ConversionChecker import ConversionChecker
 import ConversionLayout
+from datetime import datetime
+from Extractor import (
+    extract_person_names,
+    extract_practitioner_names,
+    extract_site_code,
+    get_patient,
+    get_valid_address,
+)
 
 # Converter variables
 FHIRData = ""
@@ -34,7 +42,7 @@ class Converter:
         return schemaParser
 
     # Convert data against converter schema
-    def _convertData(self, ConversionValidate, expression, dataParser):
+    def _convertData(self, ConversionValidate, expression, dataParser, json_data):
 
         FHIRFieldName = expression["fieldNameFHIR"]
         FlatFieldName = expression["fieldNameFlat"]
@@ -54,11 +62,13 @@ class Converter:
             convertedData = ConversionValidate.convertData(
                 expressionType, expressionRule, FHIRFieldName, conversionValue
             )
+            if "address" in FHIRFieldName or "performer" in FHIRFieldName or "name" in FHIRFieldName:
+                convertedData = self.extract_patient_details(json_data, FlatFieldName)
             if convertedData is not None:
                 Converted[FlatFieldName] = convertedData
 
     # run the conversion against the data
-    def runConversion(self, summarise=False, report_unexpected_exception=True):
+    def runConversion(self, json_data, summarise=False, report_unexpected_exception=True):
         try:
             dataParser = self._getFHIRParser(self.FHIRData)
         except Exception as e:
@@ -97,10 +107,32 @@ class Converter:
                 return p
 
         for conversion in conversions:
-            rows = self._convertData(ConversionValidate, conversion, dataParser)
+            rows = self._convertData(ConversionValidate, conversion, dataParser, json_data)
 
         imms.append(Converted)
         return imms
 
     def getErrorRecords(self):
         return ErrorRecords
+    
+    def extract_patient_details(self, json_data, FlatFieldName):
+        if not hasattr(self, "_cached_values"):
+            self._cached_values = {}
+
+        if not self._cached_values:
+            occurrence_time = datetime.strptime(json_data.get("occurrenceDateTime", ""), "%Y-%m-%dT%H:%M:%S%z")
+            patient = get_patient(json_data)
+            if not patient:
+                return None
+
+            self._cached_values = {
+                "PERSON_FORENAME": extract_person_names(patient, occurrence_time)[0],
+                "PERSON_SURNAME": extract_person_names(patient, occurrence_time)[1],
+                "PERSON_POSTCODE": get_valid_address(patient, occurrence_time),
+                "SITE_CODE": extract_site_code(json_data)[0],
+                "SITE_CODE_TYPE_URI": extract_site_code(json_data)[1],
+                "PERFORMING_PROFESSIONAL_FORENAME": extract_practitioner_names(json_data, occurrence_time)[0],
+                "PERFORMING_PROFESSIONAL_SURNAME": extract_practitioner_names(json_data, occurrence_time)[1]
+            }
+
+        return self._cached_values.get(FlatFieldName)
