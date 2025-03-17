b55b2cceeacbc6010a4c4a97a7170b9a9ed7cb15
CLJ2006 <clarissa.john1@nhs.net>
2025-03-17 17:44:48 +0000

    update processor tests

--- recordprocessor/tests/utils_for_recordprocessor_tests/mock_environment_variables.py
+++ recordprocessor/tests/utils_for_recordprocessor_tests/mock_environment_variables.py
@@ -23,6 +23,14 @@ class Firehose:
     STREAM_NAME = "immunisation-fhir-api-internal-dev-splunk-firehose"
 
 
+class Sqs:
+    """Class to hold SQS values for use in tests"""
+
+    ATTRIBUTES = {"FifoQueue": "true", "ContentBasedDeduplication": "true"}
+    QUEUE_NAME = "imms-batch-internal-dev-metadata-queue.fifo"
+    TEST_QUEUE_URL = f"https://sqs.{REGION_NAME}.amazonaws.com/999999999/{QUEUE_NAME}"
+
+
 MOCK_ENVIRONMENT_DICT = {
     "ENVIRONMENT": "internal-dev",
     "LOCAL_ACCOUNT_ID": "123456789012",
@@ -32,4 +40,5 @@ MOCK_ENVIRONMENT_DICT = {
     "KINESIS_STREAM_NAME": Kinesis.STREAM_NAME,
     "KINESIS_STREAM_ARN": f"arn:aws:kinesis:{REGION_NAME}:123456789012:stream/{Kinesis.STREAM_NAME}",
     "FIREHOSE_STREAM_NAME": Firehose.STREAM_NAME,
+    "AUDIT_TABLE_NAME": "immunisation-batch-internal-dev-audit-table",
 }
