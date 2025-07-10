bc67ec5ba11ef14ab82710c2a3170e6cf01c80ce
James Wharmby <218585646+JamesW1-NHS@users.noreply.github.com>
2025-07-10 14:41:57 +0100

    VED-455: Vaccine Type in Delta table is populating in lower case (#645)
    * vaccine_type -> upper case
    
    * remove comments

--- delta_backend/tests/utils_for_converter_tests.py
+++ delta_backend/tests/utils_for_converter_tests.py
@@ -179,7 +179,7 @@ class ValuesForTests:
                     "ApproximateCreationDateTime": 1690896000,
                     "NewImage": {
                         "PK": {"S": pk},
-                        "PatientSK": {"S": "COVID19#ca8ba2c6-2383-4465-b456-c1174c21cf31"},
+                        "PatientSK": {"S": "Covid19#ca8ba2c6-2383-4465-b456-c1174c21cf31"},
                         "IdentifierPK": {"S": "system#1"},
                         "Operation": {"S": operation},
                         "SupplierSystem": {"S": supplier},
@@ -195,7 +195,7 @@ class ValuesForTests:
                     "ApproximateCreationDateTime": 1690896000,
                     "Keys": {
                         "PK": {"S": pk},
-                        "PatientSK": {"S": "COVID19#ca8ba2c6-2383-4465-b456-c1174c21cf31"},
+                        "PatientSK": {"S": "Covid19#ca8ba2c6-2383-4465-b456-c1174c21cf31"},
                         "SupplierSystem": {"S": supplier},
                         "Resource": {"S": ValuesForTests.json_value_for_test},
                     }
@@ -203,7 +203,7 @@ class ValuesForTests:
             }
 
     expected_static_values = {
-        "VaccineType": "covid19",
+        "VaccineType": "COVID19",
         "SupplierSystem": "EMIS",
         "Source": "test-source",
         "ImmsID": "12345",
